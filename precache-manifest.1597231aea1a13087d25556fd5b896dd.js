6b2646c598bd94fe84d2eb42e5a77a238787385a
juanabrate <juan.abrate21@gmail.com>
2021-01-14 00:15:07 -0300

    Updates

---
+++ precache-manifest.1597231aea1a13087d25556fd5b896dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fd1141cd85fc70966c1421471d3be7",
+    "url": "/Plot-Movie-App/index.html"
+  },
+  {
+    "revision": "1d7bb4c2c6b4d62d6e97",
+    "url": "/Plot-Movie-App/static/css/main.ad2ad8f3.chunk.css"
+  },
+  {
+    "revision": "fad6dc02e567badcb319",
+    "url": "/Plot-Movie-App/static/js/2.87f800ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Plot-Movie-App/static/js/2.87f800ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d7bb4c2c6b4d62d6e97",
+    "url": "/Plot-Movie-App/static/js/main.4d8caff0.chunk.js"
+  },
+  {
+    "revision": "7624baada8fb02c70215",
+    "url": "/Plot-Movie-App/static/js/runtime-main.e4c3b928.js"
+  },
+  {
+    "revision": "37b9ee6b9b5dd01d937aa75ee78cf39b",
+    "url": "/Plot-Movie-App/static/media/kisspng-imdb-film-director-computer-icons-television-u-5ac6f594137fd3.2912686215229884360799.37b9ee6b.png"
+  }
+]);
